8bca26609b974e4315dd5e1316f0909863853467
Quinton Reeves <qreeves@gmail.com>
2018-03-18 23:07:09 +1000

    css and layout fix

--- html/index.php
+++ html/index.php
@@ -5,8 +5,20 @@
 <p>Welcome to the Red Eclipse HQ. You might be looking for the:</p>
 <p>
     <ul>
-        <li><b><a href="https://lists.redeclipse.net/">Mailing Lists</a></b></li>
-        <li><b><a href="/admin">Administrator Interface</a></b></li>
+        <li>
+            <b><a href="https://lists.redeclipse.net/">Mailing Lists</a></b>
+            <ul>
+                <li><a href="https://lists.redeclipse.net/listinfo/news">Newsletter</a></li>
+            </ul>
+        </li>
+        <li>
+            <b><a href="/admin">Administrator Interface</a></b>
+            <ul>
+                <li><a href="/admin/listusers.php">List Users</a></li>
+                <li><a href="/admin/adduser.php">Add User</a></li>
+                <li><a href="/admin/deluser.php">Delete User</a></li>
+            </ul>
+        </li>
     </ul>
 </p>
 <?php include_once("/var/www/src/footer.php"); ?>
